6200d238c71a76f1e814b079af9cbdf83ef0936f
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-06-08 14:46:34 +0530

    Create a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,22 @@
-module.exports.home = function(req, res){
-    return res.end('<h1> Express is up for Codeial! </h1>');
-}
+// module.exports.home = function(req, res){
+//     return res.end('<h1> Express is up for Codeial! </h1>');
+// }
 
 
 module.exports.account = function(req, res){
     return res.end('<h1> Accounts Section </h1>');
 }
 
-//module.exports.actionName = function(req, res){}
+module.exports.actionName = function(req, res){}
 
-// module.exports.about = function(req, res){
-//     return res.end('<h1> Express is up for About Section! </h1>')
-// }
+module.exports.about = function(req, res){
+    return res.end('<h1> Express is up for About Section! </h1>')
+}
+
+
+
+module.exports.home = function(req, res){
+   return res.render('home', {
+       title: "Home"
+   });
+}
