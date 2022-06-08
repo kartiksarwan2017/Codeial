bae8974402914fc4daf79cecb1accaf6c37e5da9
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-06-08 14:29:56 +0530

    Set Up the View Engine

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const port = 8000;
 app.use('/', require('./routes'));
 
 
+
+// Set up the view engine
+app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
+
 app.listen(port, function(err){
 
     if(err){
